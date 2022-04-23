fe6d1906c0b543026ad21ba0f8dbe6955a9f990f
Jeremy Hyrkas <hyrkas@Jeremys-MacBook-Pro.local>
2022-04-22 21:40:35 -0700

    this might work?

--- spreader.c
+++ spreader.c
@@ -1,7 +1,7 @@
 #include "ext.h" // max
 #include "ext_obex.h" // max?
-#include <math.h> // TODO: needed?
-#include <stdlib.h> // qsort()
+#include <math.h> // exp
+#include <stdlib.h> // rand
 #include <string.h> // memset()
 #ifdef NT
 #pragma warning( disable : 4244 )
@@ -12,7 +12,7 @@
 
 // copied from old ext_mess.h...bad idea??
 #define SETFLOAT(ap, x) ((ap)->a_type = A_FLOAT, (ap)->a_w.w_float = (x))
-/* ------------------------ spreader~ ----------------------------- */
+/* ------------------------ spreader ----------------------------- */
 
 static t_class *spreader_class;
 
@@ -36,6 +36,7 @@ typedef struct _spreader
     int limit;                      // current number of frequencies (and number to output)
     float curr_diss;                // dissonance of current panning
     int more_diss;                  // 1 if banging should lead to more dissonance, 0 if more consonance
+    void *bang_out;                 // output for bang when changing
     void *pan_out;                  // output for panning
 } t_spreader;
 
@@ -47,14 +48,24 @@ void set_more_diss(t_spreader *x, int t) {
 
 // function to compute current dissonance based on panning
 float get_curr_diss(t_spread *x) {
-    // TODO: implement
-    return 0.f;
+    float diss = 0.f;
+    for (int i = 0; i < x->limit; i++) {
+        for (int j = i+1; j < x->limit; j++) {
+            float distance = absf(x->workspace[i].pan - x->workspace[j].pan);
+            float scaler = 0.24f / (0.021f*minf(x->workspace[i].frequency, x->workspace[j].frequency) + 19);
+            float F = scaler * absf(x->workspace[i].frequency - x->workspace[j].frequency);
+            float diss_ = exp(-3.5f*F) - exp(-5.75*F);
+            diss += diss_;
+        }
+    }
+    return diss;
 }
 
 // this sets the frequencies and computes the current dissonance
 void set_new_freqs(t_spreader *x, t_symbol *s, long argc, t_atom *argv) {
     // argv contains (f1, f2, f3....f_n)
     int limit = argc < MAXFREQS ? argc : MAXFREQS;
+    x->limit = limit;
 
     // set frequencies
     for (int i = 0; i < limit; i++) {
@@ -77,19 +88,65 @@ void set_new_freqs(t_spreader *x, t_symbol *s, long argc, t_atom *argv) {
 
     // compute dissonance
     x->curr_diss = get_curr_diss(x);
+    
+    send_output(x, 1);
+}
 
-    // output lists regardless of bashing
-    for (int i = 0; i < limit; i++) {
+inline void send_output(t_spreader *x, int change) {
+    // output pan
+    for (int i = 0; i < x->limit; i++) {
         int osc_index = x->workspace[i].osc_index; // this aligns the inputs and outputs in case of sorting
         SETFLOAT(x->output_p+osc_index, x->workspace[i].pan);
     }
 
     outlet_list(x->pan_out, ps_list, limit, x->output_p);
+    if (change) {
+        outlet_bang(x->bang_out);
+    }
+}
+
+void randint(int limit) {
+    float r = rand() / float(RAND_MAX);
+    return int(r*limit);
 }
 
 // make an optimization step when receiving a bang
 void opt_step(t_spreader *x) {
+    if (x->limit < 3) {
+        send_output(x, 0);
+        return;
+    }
 
+    int index1 = randint(x->limit);
+    int index2 = index1;
+    while (index1 == index2) {
+        index2 = randint(x->limit);
+    }
+
+    // try a swap
+    float oldpan1 = x->workspace[index1].pan;
+    float oldpan2 = x->workspace[index2].pan;
+
+    x->workspace[index1].pan = oldpan2;
+    x->workspace[index2].pan = oldpan1;
+    float tmp_diss = get_curr_diss(x);
+    float diss_diff = tmp_diss - x->curr_diss;
+    int change = 0;
+    // change for more consonance
+    if (diss_diff < 0 && x->more_diss == 0) {
+        change = 1;
+    // change for more dissonance
+    } else if (diss_diff > 0 && x->more_diss == 1) {
+        change = 1;
+    }
+
+    // swap back if it was a worse change
+    if (change == 0) {
+        x->workspace[index1].pan = oldpan1;
+        x->workspace[index2].pan = oldpan2;
+    }
+
+    send_output(x, change);
 }
 
 // constructor
@@ -102,6 +159,7 @@ static void *spreader_new(t_symbol *s, int argc, t_atom *argv)
     int mem_size = MAXFREQS*sizeof(freq_pan);
     memset(x->workspace, 0, mem_size);
 
+    x->bang_out = bangout((t_object *)x); // add outlet
     x->pan_out = listout((t_object *)x); // add outlet
 
     // handling GIMME
